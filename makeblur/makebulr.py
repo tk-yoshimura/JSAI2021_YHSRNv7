93470181f0b5d7c19cf5a5466ea60b9a2f2a2201
tk-yoshimura <yoshimura8192@gmail.com>
2021-04-29 01:20:02 +0900

    Update .gitignore

--- makeblur/makebulr.py
+++ makeblur/makebulr.py
@@ -9,8 +9,8 @@ dirpath = 'Enter training images directory path.'
 
 np.random.seed(1234)
 
-for epoch in range(32):
-    imgpaths = glob.glob(dirpath + '{}/*.png'.format(epoch))
+for dirindex in range(32):
+    imgpaths = glob.glob(dirpath + '{}/*.png'.format(dirindex))
 
     for imgpath in imgpaths:
         sigma = random.triangular(1e-5, 2, 2e-5)
@@ -20,7 +20,7 @@ for epoch in range(32):
 
         imgname = os.path.basename(imgpath)
 
-        imgname_dst = dirpath + str(epoch) + '_blured/' + imgname
+        imgname_dst = dirpath + str(dirindex) + '_blured/' + imgname
 
         dirpath_dst = os.path.dirname(imgname_dst)
         if not os.path.exists(dirpath_dst):
